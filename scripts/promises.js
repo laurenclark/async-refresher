76c3fe3e85d991dab31d37e6cfbae77cae4a29eb
Lauren <lauren.c4231@gmail.com>
2020-06-29 00:46:37 +0100

    Add Promise.all() example

--- scripts/promises.js
+++ scripts/promises.js
@@ -36,3 +36,18 @@ createPost({
 })
     .then(getPosts)
     .catch((error) => console.error(error));
+
+// Resolve multiple promises with Promise.all()
+// const promise1 = Promise.resolve('Hello World!');
+// const promise2 = 10;
+// const promise3 = new Promise((resolve, reject) => {
+//     setTimeout(resolve, 2000, 'Bye!');
+// });
+// const promise4 = fetch(
+//     'https://jsonplaceholder.typicode.com/users'
+// ).then((res) => res.json());
+
+// // Takes as long as the longest promise, waits for resolution, and actions thenable function
+// Promise.all([promise1, promise2, promise3, promise4]).then((values) =>
+//     console.log(values)
+// );
